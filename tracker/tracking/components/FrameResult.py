a0daa819d4abe3a3ede1d47afaccdd2b0a254b03
uedawn <62166730+uedawn@users.noreply.github.com>
2023-10-28 13:24:36 +0900

    #16 FrameResultを実装

---
+++ tracker/tracking/components/FrameResult.py
@@ -0,0 +1,26 @@
+import numpy
+from Point2i import Point2i
+
+class FrameResult:
+    """
+    ある時刻(フレーム)の結果データ
+    
+    Parameters
+    ----------
+    Index : int
+        インデックス(輪番とする)
+    Position : point2i
+        フレームにおける検出物体の位置(座標系注意)
+    time : float
+        フレームの時刻(time.time()で取得した値を格納したい)
+    RawImg : numpy
+        カメラから取得した画像データそのもの
+    ResultImg : numpy
+        RawImgを処理し，物体検出結果を重ね合わせた画像データ
+    """
+    def __init__(self, index:int, position:Point2i, time:float, rawImg:numpy, resultImg:numpy):
+        self.Index = index
+        self.Position = position
+        self.Time = time
+        self.RawImg = rawImg
+        self.ResultImg = resultImg 
