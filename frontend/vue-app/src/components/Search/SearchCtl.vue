128ea20897ec6599e781a066755ec26253ee5036
Asha <62386554+asha-ndf@users.noreply.github.com>
2023-10-28 16:59:18 +0900

    検索結果表示画面の最小限度完成 (#32)
    * test
    
    * 検索結果画面テスト
    
    * 検索画面ミニマム完成

--- frontend/vue-app/src/components/Search/SearchCtl.vue
+++ frontend/vue-app/src/components/Search/SearchCtl.vue
@@ -1,44 +1,62 @@
 <template>
   <v-main>
-    <v-card v-if="beforeSearch" max-width="600" class="mx-auto">
+    <v-card max-width="600" class="mx-auto">
       <v-text-field
-        v-model="keyword"
+        v-model="state.keyword"
         append-icon="mdi-magnify"
         label="検索"
         single-line
         hide-details
       ></v-text-field>
-      <v-btn color="primary" @click="search(keyword)">検索</v-btn>
+      <v-btn color="primary" @click="search(state.keyword)">検索</v-btn>
+    </v-card>
+    <v-card v-if="beforeSearch">
+      <v-card-text>
+        <p>検索してください</p>
+      </v-card-text>
+    </v-card>
+    <v-card v-else>
+      <v-card-title>検索結果 {{ state.count }}件 - {{ state.keyword }}</v-card-title>
+      <v-card-text>
+        <v-list>
+          <v-list-item v-for="item in state.items" :key="item.id">
+              <v-list-item-title>{{ item.label }}</v-list-item-title>
+              <img v-bind:src="item.imgURL" width="300" />
+              <p>{{ item.time }} / {{ item.room_id }}</p>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
     </v-card>
   </v-main>
 </template>
   
   <script>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import axios from "axios";
 export default {
   setup() {
+    const beforeSearch = ref(true);
     const state = reactive({
       keyword: null,
       items: [],
     });
-    let beforeSearch = true;
 
     function search(keyword) {
-      beforeSearch = false;
+      this.beforeSearch = false;
       axios
-        .get(`https://example.com/api/search?q=${keyword}`)
+        .get(`https://kb2302-develop.onrender.com/search/label/v1?q=${keyword}`)
         .then((response) => {
-          state.items = response.data;
+          this.state.count = response.data.count;
+          this.state.items = response.data.contents;
+          return true;
         })
         .catch((error) => {
           console.log(error);
+          return false;
         });
     }
-
     return {
-      keyword: state.keyword,
-      items: state.items,
+      state,
       search,
       beforeSearch,
     };
