caf5e9538a1f1b94165f32061237c2f7c2aba2c9
asha-ndf <ashanomail@gmail.com>
2023-10-28 16:42:16 +0900

    検索結果画面テスト

--- frontend/vue-app/src/components/Search/SearchCtl.vue
+++ frontend/vue-app/src/components/Search/SearchCtl.vue
@@ -2,13 +2,13 @@
   <v-main>
     <v-card max-width="600" class="mx-auto">
       <v-text-field
-        v-model="keyword"
+        v-model="state.keyword"
         append-icon="mdi-magnify"
         label="検索"
         single-line
         hide-details
       ></v-text-field>
-      <v-btn color="primary" @click="search(keyword)">検索</v-btn>
+      <v-btn color="primary" @click="search(state.keyword)">検索</v-btn>
     </v-card>
     <v-card v-if="beforeSearch">
       <v-card-text>
@@ -20,9 +20,9 @@
       <v-card-text>
         <v-list>
           <v-list-item v-for="item in state.items" :key="item.id">
-            <v-list-item-content>
-              <v-list-item-title>{{ item.name }}</v-list-item-title>
-            </v-list-item-content>
+              <v-list-item-title>{{ item.label }}</v-list-item-title>
+              <img v-bind:src="item.imgURL" width="300" />
+              <p>{{ item.time }} / {{ item.room_id }}</p>
           </v-list-item>
         </v-list>
       </v-card-text>
@@ -43,10 +43,10 @@ export default {
 
     function search(keyword) {
       this.beforeSearch = false;
-      this.state.keyword = keyword;
       axios
         .get(`https://kb2302-develop.onrender.com/search/label/v1?q=${keyword}`)
         .then((response) => {
+          console.log(response.data.contents);
           this.state.count = response.data.count;
           this.state.items = response.data.contents;
         })
