ffc25cfe4f66f2bad656704642ad6cfe5a354550
falconsyunya <81752510+falconsyunya@users.noreply.github.com>
2023-10-28 16:20:36 +0900

    画像保存たたき台

--- tracker/camera/camera-driver.py
+++ tracker/camera/camera-driver.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python
+# cv2のインポート前にカメラに関する設定を行う(起動速度低下対策)
+import os
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 
 import cv2
+import datetime
 
-deviceid=0 # it depends on the order of USB connection. 
-capture = cv2.VideoCapture(deviceid)
+def save_frame(cam_num, dir):       # cam_num：0=内蔵カメラ, 1,2,...=外付けカメラ
+    cap = cv2.VideoCapture(cam_num)
 
-ret, frame = capture.read()
-cv2.imwrite('test.jpg', frame)
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('Window', frame)
+        if cv2.waitKey(33) == 27:       # 30fpsなので33ms待機, ESCキーで終了
+            break
+        strdate=datetime.datetime.now().strftime('%Y%m%dT%H%M%S%f') # 撮影時刻の生成
+        fname=dir + strdate + ".jpg"    # ファイル名の結合
+        cv2.imwrite(fname, frame)
+    cap.release()                       # メモリの開放
+    cv2.destroyAllWindows()             # 画面消去
+
+save_frame(1, 'tracker/camera/buffer/') 
