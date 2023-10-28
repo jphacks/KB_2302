cd51fe3966a59d820e6d57ee002566934c0dc852
Shinnosuke <78944240+shinnosuke1023@users.noreply.github.com>
2023-10-28 13:47:20 +0900

    backend Python実行環境構築 (#18)
    * backend Python実行環境構築
    
    * requirements.txt追加
    
    * .gitignore修正

---
+++ backend/flask-app/settings.py
@@ -0,0 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+FIREBASE_CREDENTIALS = os.environ.get("FIREBASE_CREDENTIALS")
+
