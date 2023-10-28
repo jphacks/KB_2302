6b9f923885f7ed1a260200319777794ceb5a3c25
Shinnosuke <78944240+shinnosuke1023@users.noreply.github.com>
2023-10-28 15:59:39 +0900

    firebaseにuploadする機能を実装 (#26)

--- backend/flask-app/run.py
+++ backend/flask-app/run.py
@@ -4,43 +4,39 @@ from flask import Flask, jsonify, request
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import storage
 
 
 cred = credentials.Certificate(json.loads(settings.FIREBASE_CREDENTIALS))
-firebase_admin.initialize_app(cred)
+app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-docs = db.collection('test').get()
+
+bucket = storage.bucket('jphacks-kb-2302.appspot.com', app=app)
+
+docs = db.collection('lost_objects').get()
 print(docs)
 for doc in docs:
     print(doc.to_dict())
 
+def upload_image(file, filename):
+    blob = bucket.blob(f"images/{filename}")
+    blob.upload_from_file(file, content_type="image/jpeg")
+    blob.make_public()
+    return blob.public_url
 
-
-"""
-app = Flask(__name__)
-
-# Initialize Firestore and Storage clients
-db = firestore.Client()
-bucket = storage.Client().bucket('your-bucket-name')
-
-@app.route('/api/documents', methods=['GET'])
-def get_documents():
-    # Get all documents from a Firestore collection
-    docs = db.collection('your-collection-name').get()
-    # Convert documents to a dictionary and return as JSON
-    return jsonify({doc.id: doc.to_dict() for doc in docs})
-
-@app.route('/api/upload', methods=['POST'])
-def upload_file():
-    # Get file from request
-    file = request.files['file']
-    # Upload file to Firebase Storage
-    blob = bucket.blob(file.filename)
-    blob.upload_from_file(file)
-    # Return URL of uploaded file
-    return jsonify({'url': blob.public_url})
+def upload_document(time, user, room_id, camera_id, label, image_url, detect_image_url, valid):
+    field = {"time": time, "user": user, "room_id": room_id, "camera_id": camera_id, "label": label, "imgURL": image_url, "detectImgURL": detect_image_url, "valid": valid}
+    update_time, doc_ref = db.collection('lost_objects').add(field)
+    print(f"{update_time}: Added document with id {doc_ref.id}")
 
 if __name__ == '__main__':
-    app.run()
-"""
+    img = ""
+    detect_img = ""
+    with open("IMG_4231.jpg", 'rb') as image:
+        img = upload_image(image, image.name)
+    with open("IMG_4231detect.jpg", 'rb') as image:
+        detect_img = upload_image(image, image.name)
+    import datetime
+    upload_document(datetime.datetime.now(), "Shin", 0, 1, "mouse", img, detect_img, True)
+
