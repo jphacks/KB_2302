e90635614a5a801471c4de2e56be536a10e5d436
asha-ndf <ashanomail@gmail.com>
2023-10-28 14:26:44 +0900

    fastapi-init

--- backend/frontback/main.py
+++ backend/frontback/main.py
@@ -12,3 +12,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
