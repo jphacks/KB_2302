22f7f208b160289e99cb72c701e942b71d2a448b
kuro0315 <44069095+kuro0315@users.noreply.github.com>
2023-10-29 04:07:46 +0900

    バックエンドにデータを送信するAPIの作成

--- tracker/tracking/components/FormData.py
+++ tracker/tracking/components/FormData.py
@@ -1,4 +1,39 @@
-class FormData():
+import cv2
+import io
+import requests
+import asyncio
+import aiohttp
+
+url = "https://kb2302-jphack-471e16480034.herokuapp.com/tracker/lost"
+
+def postData(
+        label:str,
+        rawimg,
+        detectimg,
+        user:str = "SSSRC",
+        room_id:int = 1,
+        camera_id:int = 1,
+    ):
+    
+    asyncio.run(
+        __postData(
+            label=label,
+            rawimg=rawimg,
+            detectimg=detectimg,
+            user=user,
+            room_id=room_id,
+            camera_id=camera_id,
+            )
+        )
+
+async def __postData(
+        label:str,
+        rawimg,
+        detectimg,
+        user:str = "SSSRC",
+        room_id:int = 1,
+        camera_id:int = 1,
+    ):
     """
     DBに渡す形
     
@@ -6,9 +41,9 @@ class FormData():
     ----------
     Usr : str
         username
-    Roomid : int
+    room_id : int
         room number
-    Cameraid : int
+    camera_id : int
         camera number
     Label : str
         label name
@@ -17,10 +52,30 @@ class FormData():
     DetectImg : 
         RawImgを処理した画像データ
     """
-    def __init__(self,user:str,roomid:int,cameraid:int,label:str,rawimg,detectimg):
-        self.User=user
-        self.Roomid=roomid
-        self.Cameraid=cameraid
-        self.Label=label
-        self.RawImg=rawimg
-        self.DetectImg=detectimg
+    data = {
+        "user": user,
+        "room_id": room_id,
+        "camera_id": camera_id,
+        "label": label
+    }
+    files = {"img": rawimg, "detect_img": rawimg}
+    
+    # Send the POST request
+    response = requests.post(url, data=data, files=files)
+    
+
+if __name__ == "__main__":
+    # テスト用
+    url = "http://127.0.0.1:5000/tracker/lost"
+    
+    img = open("../zidane.jpg" ,"rb")
+    detectimg = open("../zidane.jpg", "rb")
+    
+    # Encode the image as a JPEG byte stream
+    byte_stream = io.BytesIO(img.read())
+    
+    postData(
+        "pen case", 
+        img, 
+        img
+    )
