657f1312c297120378376d42f859bf35d28ebfc3
asha-ndf <ashanomail@gmail.com>
2023-10-28 11:14:27 +0900

    vue add vuetify

--- frontend/vue-app/src/main.js
+++ frontend/vue-app/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
